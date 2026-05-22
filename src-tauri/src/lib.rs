fdb61e98b1c3cc0d6d3208175ceae835b7689cca
chushi-chef <cbs124739@gmail.com>
2026-05-23 02:45:56 +0800

    feat: enforce single app instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ use std::{
     process::Command,
     time::{SystemTime, UNIX_EPOCH},
 };
+use tauri::Manager;
 use thiserror::Error;
 
 #[cfg(windows)]
@@ -1497,6 +1498,13 @@ fn activate_profile(
 
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.unminimize();
+                let _ = window.show();
+                let _ = window.set_focus();
+            }
+        }))
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
